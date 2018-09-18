b379f4729a6dbc0399fc4e85dd1084a4ff7e3146
balwinder <balwinder@10.1.249.211>
2018-09-18 14:15:39 +0530

    Updated Runner Class Steps Definition path in glue

--- Cucumber/src/test/java/com/dhillon/Cucumber/runner/MainRunner.java
+++ Cucumber/src/test/java/com/dhillon/Cucumber/runner/MainRunner.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(		
 		features = {"C:\\Users\\balwinder\\git\\Cucumber\\Cucumber\\src\\test\\java\\com\\dhillon\\Cucumber\\features\\Login.feature","C:\\Users\\balwinder\\git\\Cucumber\\Cucumber\\src\\test\\java\\com\\dhillon\\Cucumber\\features\\Login2.feature"},
-		glue = {"C:\\Users\\balwinder\\git\\Cucumber\\Cucumber\\src\\test\\java\\com\\dhillon\\Cucumber\\steps\\"},
+		glue = {"com.dhillon.Cucumber.steps"},
 		monochrome =true,
 		tags={},
 		plugin={"pretty" , "html:target/cucumber","json:target/cucumber.json","com.cucumber.listener.ExtentCucumberFormatter:target/report.html"}
