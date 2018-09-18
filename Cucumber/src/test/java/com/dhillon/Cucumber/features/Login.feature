fbc76850bd538ea940e5a460ad5b31c7efbc21be
balwinder <balwinder@10.1.249.211>
2018-09-18 18:41:39 +0530

    Updated Resources Folder

--- Cucumber/src/test/java/com/dhillon/Cucumber/features/Login.feature
+++ Cucumber/src/test/java/com/dhillon/Cucumber/features/Login.feature
@@ -5,7 +5,7 @@ Scenario: Login in Account with valid credentials.
 	Given Balwinder navigate to the stackoverflow website on the login page 
 	When Balwinder clicks on the login button 
 	And Balwinder enters valid username ,password 
-	And clicks on the login page. 
+	And clicks on the Submit button.  
 	Then System should allow Balwinder to login successfully.
 	
 	
--- Cucumber/src/test/java/com/dhillon/Cucumber/steps/Login.java
+++ Cucumber/src/test/java/com/dhillon/Cucumber/steps/Login.java
@@ -1,38 +1,61 @@
 package com.dhillon.Cucumber.steps;
 
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Login {
+
+	WebDriver driver;
+
+	@Before
+	public void setUp() {
+		System.setProperty(
+				"webdriver.gecko.driver",
+				"C:\\Users\\balwinder\\git\\Cucumber\\Cucumber\\src\\test\\java\\com\\bdhillon\\Cucumber\\resoruces\\geckodriver.exe");
+		this.driver = new FirefoxDriver();
+		//this.driver.manage().window().maximize();
+		this.driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+	}
+
 	@Given("^Balwinder navigate to the stackoverflow website on the login page$")
-	public void balwinder_navigate_to_the_stackoverflow_website_on_the_login_page() throws Throwable {
-	   System.out.println("Balwinder step Login Feature");
+	public void balwinder_navigate_to_the_stackoverflow_website_on_the_login_page()
+			throws Throwable {
+		driver.navigate().to("https://stackoverflow.com/");
+		
 	}
 
 	@When("^Balwinder clicks on the login button$")
 	public void balwinder_clicks_on_the_login_button() throws Throwable {
-		 System.out.println("Balwinder step Login Feature");
+		driver.findElement(By.xpath("//a[text()='Log In']")).click();
 	}
 
 	@When("^Balwinder enters valid username ,password$")
 	public void balwinder_enters_valid_username_password() throws Throwable {
-		 System.out.println("Balwinder step Login Feature");
+		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("Balvinder.dh23@gmail.com");
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Blue@4521");
 	}
 
-	@When("^clicks on the login page\\.$")
-	public void clicks_on_the_login_page() throws Throwable {
-		 System.out.println("Balwinder step Login Feature");
+	@When("^clicks on the Submit button\\.$")
+	public void clicks_on_the_Submit_button() throws Throwable {
+		driver.findElement(By.id("submit-button")).click();
 	}
-
+	
 	@Then("^System should allow Balwinder to login successfully\\.$")
-	public void system_should_allow_Balwinder_to_login_successfully() throws Throwable {
-		 System.out.println("Balwinder step Login Feature");
-	}
-
-
+	public void system_should_allow_Balwinder_to_login_successfully()
+			throws Throwable {
+		driver.wait(60);;
+		boolean b=driver.findElement(By.xpath("//div[@title='balvinder dhillon']")).isDisplayed();
+		System.out.println(b);
+		driver.quit();
 		
-
+	}
 
 }
